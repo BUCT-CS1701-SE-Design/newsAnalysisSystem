f6e09d35eda37c06ee1ce1617f205ffa4bd67ace
LSSxyz <xshlishuang@163.com>
2020-05-28 16:36:59 +0800

    细化数据定制和情感分析

--- project/main.py
+++ project/main.py
@@ -5,14 +5,22 @@ import re
 import pandas as pd
 import numpy as np
 import os
-from sense import Analyse,train
+from sense import Analyse
 from sqlalchemy import create_engine
 import pymysql
+
+import datetime
+
+def to_date(s):
+    # print(s)
+    y,m,d=s.split("-")
+    date=datetime.date(int(y),int(m),int(d))
+    return date
 
 def read_file():
     res=[]
     current_path = os.path.dirname(__file__)
-    names=pd.read_csv(current_path+'names.csv')
+    names=pd.read_csv(current_path+'names.csv') # 此处如果本地运行需要写成 current_path+'\\names.csv'格式
     for x in names['name']:
         res.append(x)
     return res
@@ -40,6 +48,7 @@ def get_news(news,idx):
         time=time.replace('月','-')
         time=time.replace('日','')
         time=time[-16::]
+        # time=time[:11:]
 
         title0=soup.find('h1')
         title=None
@@ -79,7 +88,7 @@ def get_news(news,idx):
 
 # get_news('http://news.cctv.com/2019/09/09/ARTISfcJJbpt2x9WBMeTUFTS190909.shtml',1)
 
-def get_url(name):
+def get_url(name,time_id):
     urls=[]
     imgs=[]
     num=1
@@ -88,15 +97,15 @@ def get_url(name):
             hd={'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1.6)', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}
             kv={
                 'qtext': name,
-                'sort':'relevance',
+                'sort':'date',
                 'type':'web',
-                'datepid':'5',
+                'datepid':str(time_id),
                 'channel':'新闻',
                 'page': str(num)
             }
             r=requests.get('https://search.cctv.com/search.php',headers=hd,params=kv)
             # print(r.text)
-            # r.raise_for_status()
+            r.raise_for_status()
             r.encoding=r.apparent_encoding
             soup=BeautifulSoup(r.text,'html.parser')
             comments = soup.findAll(text=lambda text: isinstance(text, Comment))
@@ -129,7 +138,7 @@ def get_url(name):
             if num>tot:
                 break
         except:
-        #     print('fuck')
+            # print('fuck')
             return urls,imgs
     return urls,imgs
 
@@ -139,9 +148,13 @@ def check(x):
     else:
         return 'positive'
 
-def main():
+#time_id: 1为全部 2为一周内 3为一个月内 4为半年内 5为一年内 默认为全部
+#museum_name:指定博物馆 默认为全部
+#start_time,end_time:指定时间段
+def get_resualt(museum_name='',start_time='1949-10-01',end_time='2049-10-01',time_id=1):
     titles=[]
     tt=[]
+    names=[]
     names=read_file()
     data={
         'newsID':[],
@@ -162,22 +175,35 @@ def main():
     xxxx=cnt[0][0]
     if xxxx==None:xxxx=0
     print('x=',xxxx)
-
     for i,name in enumerate(names):
+        if not museum_name=='':
+            if not name==museum_name:
+                continue
         idx=i+1
         print(name, idx)
-        urls,imgs=get_url(name)
+        urls,imgs=get_url(name,time_id)
+        # print(urls)
         for x,imgx in zip(urls,imgs):
             xxxx+=1
             xx=get_news(x,idx)
             if xx['id']==None or xx['title']==None or xx['content']==None or xx['time']==None:
                 tt.append(x)
                 continue
-            # 判断是否已经在库中
-            # print("title=", xx['title'])
+            
+            timex=str(xx['time'])
+            datex=timex[:11:]
+            time_re=re.compile(r'\d\d\d\d-\d\d-\d\d')
+            if not time_re.match(timex):
+                continue
+            if to_date(start_time)>to_date(datex) or to_date(end_time)<to_date(datex):
+                continue
+            print('start_time=', start_time, 'end_time=', end_time, 'timex=', timex)
+
+            #判断是否已经在库中
+            print("title=", xx['title'])
             sql="select * from museumnews where newsTitle='%s' and museumID='%s'" % (xx['title'],xx['id'])
             exist = cursor.execute(sql)
-            print('exist=', exist, ' title=', xx['title'], ' time=', xx['time'], 'xxxx=', xxxx)
+            print('exist=', exist, ' title=', xx['title'], ' time=', xx['time'],)
             if exist:
                 xxxx-=1
             else:
@@ -186,9 +212,10 @@ def main():
                 data['museumID'].append(xx['id'])
                 data['newsTitle'].append(xx['title'])
                 data['newsmaintext'].append(xx['content'])
-                data['newsTime'].append(xx['time'])
+                data['newsTime'].append(str(xx['time']))
                 data['imageurl'].append(imgx)
                 titles.append(xx['title'])
+
         #     if xxxx>=3:
         #         break
         # if xxxx>=3:
@@ -204,4 +231,4 @@ def main():
     # df.to_csv(current_path+'\\data.csv',encoding='utf_8_sig')
     # write_file(titles)
 
-main()
+get_resualt(museum_name=m,start_time=s,end_time=e)
